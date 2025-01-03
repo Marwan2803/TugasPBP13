9cc98d154fccd024e56f3c9ee63cacbf4de86505
Marwan2803 <marwanmuharam207@gmail.com>
2025-01-03 10:39:30 +0700

    Update global.test.js

--- Test/global.test.js
+++ Test/global.test.js
@@ -3,86 +3,87 @@ const app = require("../index");
 const Prisma = require("../prisma/client");
 
 beforeAll(async () => {
-  await Prisma.ligabelanda.deleteMany();
-  await Prisma.ligainggris.deleteMany();
-  await Prisma.ligajerman.deleteMany();
-  await Prisma.ligaprancis.deleteMany();
-  await Prisma.ligaspanyol.deleteMany();
+  await Prisma.actionMovies.deleteMany();
+  await Prisma.comedyMovies.deleteMany();
+  await Prisma.dramaMovies.deleteMany();
+  await Prisma.horrorMovies.deleteMany();
+  await Prisma.sciFiMovies.deleteMany();
 });
 
 afterAll(async () => {
   await Prisma.$disconnect();
 });
 
-describe("Liga API Endpoints", () => {
-  const liga = "ligainggris";
+describe("Movies API Endpoints", () => {
+  const genre = "actionMovies";
   let id;
 
-  test("GET /ligabelanda - Read Footballers Data", async () => {
-    const response = await request(app).get("/ligabelanda");
+  test("GET /actionMovies - Read Action Movies Data", async () => {
+    const response = await request(app).get("/actionMovies");
     expect(response.statusCode).toBe(200);
     expect(response.body).toHaveProperty("message");
-    expect(response.body).toHaveProperty("footballers");
-  });
-  test("GET /ligainggris - Read Footballers Data", async () => {
-    const response = await request(app).get("/ligainggris");
-    expect(response.statusCode).toBe(200);
-    expect(response.body).toHaveProperty("message");
-    expect(response.body).toHaveProperty("footballers");
-  });
-  test("GET /ligajerman - Read Footballers Data", async () => {
-    const response = await request(app).get("/ligajerman");
-    expect(response.statusCode).toBe(200);
-    expect(response.body).toHaveProperty("message");
-    expect(response.body).toHaveProperty("footballers");
-  });
-  test("GET /ligaprancis - Read Footballers Data", async () => {
-    const response = await request(app).get("/ligaprancis");
-    expect(response.statusCode).toBe(200);
-    expect(response.body).toHaveProperty("message");
-    expect(response.body).toHaveProperty("footballers");
-  });
-  test("GET /ligaspanyol - Read Footballers Data", async () => {
-    const response = await request(app).get("/ligaspanyol");
-    expect(response.statusCode).toBe(200);
-    expect(response.body).toHaveProperty("message");
-    expect(response.body).toHaveProperty("footballers");
+    expect(response.body).toHaveProperty("movies");
   });
 
-  test("POST /pemain/:liga", async () => {
-    const response = await request(app).post(`/pemain/${liga}`).send({
-      nama: "Erling Haaland",
-      umur: 20,
-      posisi: "CF",
-      NA: "Denmark",
-      KA: "Borussia Dortmund",
-      KSI: "Manchester City",
-      harga: "1000000",
+  test("GET /comedyMovies - Read Comedy Movies Data", async () => {
+    const response = await request(app).get("/comedyMovies");
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toHaveProperty("message");
+    expect(response.body).toHaveProperty("movies");
+  });
+
+  test("GET /dramaMovies - Read Drama Movies Data", async () => {
+    const response = await request(app).get("/dramaMovies");
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toHaveProperty("message");
+    expect(response.body).toHaveProperty("movies");
+  });
+
+  test("GET /horrorMovies - Read Horror Movies Data", async () => {
+    const response = await request(app).get("/horrorMovies");
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toHaveProperty("message");
+    expect(response.body).toHaveProperty("movies");
+  });
+
+  test("GET /sciFiMovies - Read Sci-Fi Movies Data", async () => {
+    const response = await request(app).get("/sciFiMovies");
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toHaveProperty("message");
+    expect(response.body).toHaveProperty("movies");
+  });
+
+  test("POST /movies/:genre", async () => {
+    const response = await request(app).post(`/movies/${genre}`).send({
+      title: "Inception",
+      director: "Christopher Nolan",
+      releaseYear: 2010,
+      genre: "Action",
+      rating: 8.8,
     });
     expect(response.statusCode).toBe(201);
     expect(response.body).toHaveProperty("message");
-    expect(response.body).toHaveProperty("insertFootballer");
-    id = response.body.insertFootballer.insertedFootballer.id;
+    expect(response.body).toHaveProperty("insertMovie");
+    id = response.body.insertMovie.insertedMovie.id;
   });
 
-  test("PUT /pemain/:liga/:id ", async () => {
-    const response = await request(app).put(`/pemain/${liga}/${id}`).send({
-      nama: "Erling Haaland",
-      umur: 25,
-      posisi: "CF",
-      NA: "Denmark",
-      KA: "Borussia Dortmund",
-      KSI: "Manchester City",
-      harga: "1000000",
+  test("PUT /movies/:genre/:id ", async () => {
+    const response = await request(app).put(`/movies/${genre}/${id}`).send({
+      title: "Inception",
+      director: "Christopher Nolan",
+      releaseYear: 2010,
+      genre: "Action",
+      rating: 9.0,
     });
     expect(response.statusCode).toBe(200);
     expect(response.body).toHaveProperty("message");
   });
 
-  test("DELETE /pemain/:liga/:id - Menghapus Data Pemain", async () => {
-    const response = await request(app).delete(`/pemain/${liga}/${id}`);
+  test("DELETE /movies/:genre/:id - Delete Movie Data", async () => {
+    const response = await request(app).delete(`/movies/${genre}/${id}`);
 
     expect(response.statusCode).toBe(202);
     expect(response.body).toHaveProperty("message");
   });
 });
+                                         
